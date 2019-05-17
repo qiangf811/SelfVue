bb1f1eebfd45b384e9bf6878912f2be2487ef6ca
qiangfeng <qiangfeng@tunicorn.cn>
2019-05-17 15:33:42 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
+// import $ from 'jquery'
+require('jquery')
+const jsonp = require('jsonp')
 const SelfVue = require('./selfVue')
 var ele = document.querySelector('#name')
 var selfVue = new SelfVue({
-  name: 'hello-world '
+  name: 'hello-worldssss '
 }, ele, 'name')
 window.setTimeout(function () {
   selfVue.name = 'change'
+  jsonp('http://www.fengleaf.cn/api/skills',function(err, data){
+    console.log(err, data)
+  })
 }, 2000)
+/**
+ * 图片懒加载
+ */
+var aImg = document.querySelectorAll('img')
+var len = aImg.length
+var n = 0; // 存储图片加载到的位置，避免每次都从第一张图片开始遍历
+window.onscroll = function () {
+  var seeHeight = document.documentElement.clientHeight
+  var scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+  for (var i = n; i < len; i++) {
+    if (aImg[i].offsetTop < seeHeight + scrollTop) {
+      if (aImg[i].getAttribute('src') == '') {
+        aImg[i].src = aImg[i].getAttribute('data-src')
+      }
+      n = i + 1
+      console.log('n = ' + n)
+    }
+  }
+}
+window.onscroll()
+
+var ff = function(){}
+Promise.try(ff({id: ''}))
+  .then(()=>{
+	console.log('then')
+})
+  .catch(e=>{
+console.log(e)
+	})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
 const env = process.env.NODE_ENV
 const isDev = process.env.NODE_ENV === 'development'
-
+// console.log(process.env)
 const config = {
   target: 'web',
   entry: ['./main.js'],
@@ -11,11 +13,20 @@ const config = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [{
+      test: require.resolve('jquery'), // require.resolve 用来获取模块的绝对路径
+      use: [{
+        loader: 'expose-loader',
+        options: 'jQuery'
+      }, {
+        loader: 'expose-loader',
+        options: '$'
+      }]
+    }]
+  },
   mode: env,
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': env
-    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
@@ -34,6 +45,10 @@ if (isDev) {
     hot: true
   }
   config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.evn': '"development"'
+    }),
+    new FriendlyErrorsPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   )
